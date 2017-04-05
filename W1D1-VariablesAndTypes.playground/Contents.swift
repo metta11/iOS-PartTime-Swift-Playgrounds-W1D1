4220908f8f6439386dee2131960817d5830d12fd
Metaworld <meta.vodopivc@gmail.com>
2017-04-04 20:47:46 -0400

    Challenge 2 completed
    Completed exercise 2-bonus

--- W1D1-VariablesAndTypes.playground/Contents.swift
+++ W1D1-VariablesAndTypes.playground/Contents.swift
@@ -47,6 +47,7 @@ greeting = "hurrreeey"
 //: ### Challenge 2
 //:
 //: We want to add 1 to whatever the value of `count` is now. We could manually look at the value in `count`, see that it is 10 and put 11 into `count` using the assignment operator. This works, but we can do it in a better way. We can set count to be whatever count is, plus 1. See if you can figure out the syntax for this.
+count = 11
 
 
 //: *Note:* If you're ever unsure of the contents of a variable in a playground, write the variable on a line by itself and you will see its value printed to the right of it. Or you can use the `print` function like so: `print(variableName)`.
@@ -75,11 +76,11 @@ count
 //: What type are the variables `tipAmount`, and `isOpen`?
 
 var tipAmount = 6.01
-// tipAmount's type is:
+// tipAmount's type is:double
 
 
 var isOpen = true
-// isOpen's type is:
+// isOpen's type is: bool
 
 
 //: ## Type Inference
@@ -89,10 +90,11 @@ var isOpen = true
 //:
 //: To create a Double, all we need to do is specify the type when we create the variable.
 
-var height: Double = 10
+var height: Double = 10.09
 
 //: Since all our variables have types, even if we haven't specifically set them, we can't store any value in them... only values of the right type.
 //: Test this out. Try setting the value of `count` to be `height`:
+//height = count
 
 
 
@@ -109,6 +111,8 @@ var height: Double = 10
 
 var schoolName = "Lightouse Labs"
 
+schoolName.uppercased()
+schoolName.lowercased()
 
 
 //: THE END
